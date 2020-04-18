f23c48222b000e7c2bd2f6e93d3b34c63f85b967
Meng Yang <mengyang41@gmail.com>
2020-04-18 17:33:23 -0400

    update

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ $('#inputbtn').click(() => {
 
     function getCoord () {
         return new Promise((resolve, reject) => {
-            $.get("http://api.openweathermap.org/data/2.5/weather?zip="+zipcode+",us&APPID=36de89dd9ba1aaa422fa4d99ab092bef", (response) => {
+            $.get("https://api.openweathermap.org/data/2.5/weather?zip="+zipcode+",us&APPID=36de89dd9ba1aaa422fa4d99ab092bef", (response) => {
                 //console.log(response);
                 lat = response.coord.lat;
                 long = response.coord.lon;
@@ -29,23 +29,23 @@ $('#inputbtn').click(() => {
 
             switch(response.current.weather[0].main) {
                 case "Clear": 
-                    $('#picbox').html("<img src='https://media.giphy.com/media/QZz9r30N2RM7DFIXqE/source.gif'>")
+                    $('#datebox').append("<img src='https://media.giphy.com/media/QZz9r30N2RM7DFIXqE/source.gif'>")
                     break;
                 case "Thunderstorm": 
-                    $('#picbox').html("<img src='https://media.giphy.com/media/xUOwGoNa2uX6M170d2/source.gif'>")
+                    $('#datebox').append("<img src='https://media.giphy.com/media/xUOwGoNa2uX6M170d2/source.gif'>")
                     break;
                 case "Drizzle":
                 case "Rain":
-                    $('#picbox').html("<img src='https://media.giphy.com/media/U7yxnzr21Xhnnb7Zxz/source.gif'>")
+                    $('#datebox').append("<img src='https://media.giphy.com/media/U7yxnzr21Xhnnb7Zxz/source.gif'>")
                     break;
                 case "Snow":
-                    $('#picbox').html("<img src='https://media.giphy.com/media/6YNgoTEPs6vZe/source.gif'>")
+                    $('#datebox').append("<img src='https://media.giphy.com/media/6YNgoTEPs6vZe/source.gif'>")
                     break;
                 case "Clouds":
-                    $('#picbox').html("<img src='https://media.giphy.com/media/Ke7i5t6QDmDSO82Uga/source.gif'>")
+                    $('#datebox').append("<img src='https://media.giphy.com/media/Ke7i5t6QDmDSO82Uga/source.gif'>")
                     break;
                 default:
-                    $('#picbox').html("<img src='https://media.giphy.com/media/xUOxfjsW9fWPqEWouI/source.gif'>")
+                    $('#datebox').append("<img src='https://media.giphy.com/media/xUOxfjsW9fWPqEWouI/source.gif'>")
                     break;
             };
         }); 
